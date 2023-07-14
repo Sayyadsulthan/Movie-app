8bd9a01973c98ea1cce9cb0aca1619db15f288f9
Sayyadsulthan <asulthan088@gmail.com>
2023-07-14 23:18:08 +0530

    Dispatching actions

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ import App from "./components/App";
 import movie from "./reducers";
 
 const store = createStore(movie);
-console.log("store",store)
-console.log("state",store.getState())
+console.log("store", store);
+console.log("BEFORE state", store.getState());
+
+store.dispatch({
+  type: "Add_Movie",
+  movies: [{ title: "sayyad" }],
+});
+
+console.log("AFTER state", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
